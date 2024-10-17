b03ca6d68c04560c03712e39225cbdff19e98b14
rahul181203 <rahulkanakam123@gmail.com>
2024-10-17 13:19:04 +0530

    error resolved

--- src/app/api/ai/route.ts
+++
@@ -1,9 +0,0 @@
-import { GetReply } from "@/services/ai";
-import { NextRequest, NextResponse } from "next/server";
-
-
-export async function POST(req:Request){
-    const question = await req.json();
-    const data = await GetReply(question.question)
-    return NextResponse.json(data)
-}
